44a44a9df9aafa02f18962154a46116c66709b1c
Michael Roebuck <michaelr@tradesy.com>
2022-03-29 16:45:42 -0600

    Create a workspace & project.

---
+++ Tradesy KMM iOS Trial/Tradesy KMM iOS Trial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tradesy KMM iOS Trial
+//
+//  Created by Michael Roebuck on 3/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tradesy KMM iOS Trial/Tradesy KMM iOS Trial/Tradesy_KMM_iOS_TrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tradesy_KMM_iOS_TrialApp.swift
+//  Tradesy KMM iOS Trial
+//
+//  Created by Michael Roebuck on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Tradesy_KMM_iOS_TrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
